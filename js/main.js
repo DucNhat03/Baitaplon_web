1fdec9d732de367267f0b46967f56da4a95ab3e7
DucNhat <cuem8h@gmail.com>
2023-05-06 22:12:59 +0700

    UpdateJS: add func for button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+
+//Open link in new tab
 function openLink(hreff)
 {
-    return location.href = hreff;
+    // return location.href = hreff;
+    return window.open(hreff,"_blank")
 } 
-
+//Open link in my tab
+function openLink1(hreff)
+{
+    return window.open(hreff,"_self")
+} 
+//Reload myweb
 function reLoad()
 {
     return location.reload();
